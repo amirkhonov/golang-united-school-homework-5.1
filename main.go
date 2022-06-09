11d68a8ef0dc19b5f870720ac969b0914a0b8b8b
Akbarkhon Amirkhonov <8295502+amirkhonov@users.noreply.github.com>
2022-06-09 17:27:43 +0500

    Add squaer

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+type Point struct {
+	x, y int
+}
+
+type Square struct {
+	start Point
+	a     uint
+}
+
+func (s Square) End() Point {
+	return Point{
+		x: s.start.x + int(s.a),
+		y: s.start.y + int(s.a),
+	}
+}
+
+func (s Square) Area() uint {
+	// implement me
+	return s.a * s.a
+}
+
+func (s Square) Perimeter() uint {
+	// implement me
+	return s.a * 4
+}
